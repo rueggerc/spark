df4003d6534cda511616d7dd547c95fdbd8b87bd
Chris <chris.ruegger@gmail.com>
2017-12-17 17:00:24 -0500

    Added Apps

---
+++ MySpark/src/main/java/com/rueggerllc/spark/apps/CountExample.java
@@ -0,0 +1,39 @@
+package com.rueggerllc.spark.apps;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class CountExample {
+	
+	private static final Logger logger = Logger.getLogger(CountExample.class);
+
+    public static void main(String[] args) throws Exception {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        
+        logger.info("==== CountExample BEGIN ====");
+        
+        SparkConf conf = new SparkConf().setAppName("count").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<String> inputWords = Arrays.asList("spark", "hadoop", "spark", "hive", "pig", "cassandra", "hadoop");
+        JavaRDD<String> wordRdd = sc.parallelize(inputWords);
+
+        logger.info("Count: " + wordRdd.count());
+
+        Map<String, Long> wordCountByValue = wordRdd.countByValue();
+
+        logger.info("CountByValue:");
+
+        for (Map.Entry<String, Long> entry : wordCountByValue.entrySet()) {
+            logger.info(entry.getKey() + " : " + entry.getValue());
+        }
+        logger.info("==== CountExample END ====");
+    }
+}
---
+++ MySpark/src/main/java/com/rueggerllc/spark/apps/MyExampleHDFS.java
@@ -0,0 +1,54 @@
+package com.rueggerllc.spark.apps;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class MyExampleHDFS {
+	
+	private static final Logger logger = Logger.getLogger(MyExampleHDFS.class);
+
+    public static void main(String[] args) throws Exception {
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        
+        logger.info("==== MyExampleHDFS BEGIN Version 2.0 ====");
+        
+//	    SparkConf conf = new SparkConf().setAppName("MyExampleHDFS")
+//		         .setMaster("spark://192.168.243.128:7077")
+//		         .set("spark.akka.heartbeat.interval", "100")
+//		         .set("spark.local.ip", "127.0.0.1");
+	    
+	    SparkConf conf = new SparkConf().setAppName("mySparkHDFS").setMaster("spark://192.168.243.128:7077");
+	    JavaSparkContext sc = new JavaSparkContext(conf);
+	    
+	    logger.info("Context Created");
+        
+	    JavaRDD<String> lines = sc.textFile("hdfs://192.168.243.128:9000/my_storage/word_count.text");
+	    JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split(" ")).iterator());
+
+//        Map<String, Long> wordCountByValue = words.countByValue();
+//        logger.info("CountByValue:");
+//
+//        for (Map.Entry<String, Long> entry : wordCountByValue.entrySet()) {
+//            logger.info(entry.getKey() + " : " + entry.getValue());
+//        }
+	    
+	    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+	    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+        
+		counts.saveAsTextFile("hdfs://192.168.243.128:9000/my_output/");
+		
+        if (sc != null) {
+        	sc.close();
+        }
+        
+        logger.info("==== MyExampleHDFS END ====");
+    }
+}
