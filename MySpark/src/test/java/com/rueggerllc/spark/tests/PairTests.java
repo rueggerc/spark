22377cfd6dee63d1760d9f24fbed342e12475c24
Chris <chris.ruegger@gmail.com>
2017-12-25 16:13:08 -0500

    Added PairRDD Tests

--- MySpark/src/test/java/com/rueggerllc/spark/tests/PairTests.java
+++ MySpark/src/test/java/com/rueggerllc/spark/tests/PairTests.java
@@ -3,20 +3,22 @@ package com.rueggerllc.spark.tests;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.storage.StorageLevel;
+import org.apache.spark.api.java.function.PairFunction;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import scala.Tuple2;
 
 public class PairTests {
 
@@ -55,31 +57,99 @@ public class PairTests {
 	
 	@Test
 	@Ignore
-	public void testPersistToMemory() {
+	public void testPairRDD1() {
 		try {
 			
 			Logger.getLogger("org").setLevel(Level.ERROR);
-			logger.info("=== SUM OF PRIMES TEST BEGIN");
+			logger.info("=== PAIR RDD TEST BEGIN");
 			
-	        SparkConf conf = new SparkConf().setAppName("persistToMemory").setMaster("local[*]");
+	        SparkConf conf = new SparkConf().setAppName("testPairRDD1").setMaster("local[*]");
 	        JavaSparkContext sc = new JavaSparkContext(conf);
+	        
+	        List<Tuple2<String,Integer>> namesAndAges = new ArrayList<Tuple2<String,Integer>>();
+	        namesAndAges.add(new Tuple2<>("Data", 12));
+	        namesAndAges.add(new Tuple2<>("Fred", 43));
+	        namesAndAges.add(new Tuple2<>("Barney", 39));
+	        namesAndAges.add(new Tuple2<>("Wilma", 37));
+	        JavaPairRDD<String, Integer> nameAgeRDD = sc.parallelizePairs(namesAndAges);
+	        nameAgeRDD.foreach(entry -> {
+	        	logger.info("Name=" + entry._1() + " Age=" + entry._2);
+	        });
+	        
+	        nameAgeRDD.coalesce(1).saveAsTextFile("output/tuples.out");
+	        
 
-	        JavaRDD<String> lines = sc.textFile("input/prime_nums.text");
-	        JavaRDD<String> numbers = lines.flatMap(line -> Arrays.asList(line.split("\\s+")).iterator());
+//	        List<Tuple2<String, Integer>> tuples = Arrays.asList(new Tuple2<>("Lily", 23),
+//                    new Tuple2<>("Jack", 29),
+//                    new Tuple2<>("Mary", 29),
+//                    new Tuple2<>("James",8));
+//
+//	        JavaPairRDD<String, Integer> pairRDD = sc.parallelizePairs(tuples);
+//		    pairRDD.foreach(data -> {
+//		        logger.info("name="+data._1() + " age=" + data._2());
+//		    }); 	        
 
-	        JavaRDD<String> validNumbers = numbers.filter(number -> !number.isEmpty());
-
-	        JavaRDD<Integer> intNumbers = validNumbers.map(number -> Integer.valueOf(number));
-
-	        logger.info("Sum is: " + intNumbers.reduce((x, y) -> x + y));
-
-			logger.info("=== SUM OF PRIMES TEST END");
+			logger.info("=== PAIR RDD TEST END");
 			
 
 		} catch (Exception e) {
 			logger.error("Error", e);
 		}
 	}
+	
+	@Test
+	// @Ignore
+	public void testConvertToPairRDD() {
+		try {
+			
+			Logger.getLogger("org").setLevel(Level.ERROR);
+			logger.info("=== CONVERT TO PAIR RDD TEST BEGIN");
+			
+	        SparkConf conf = new SparkConf().setAppName("testPairRDD1").setMaster("local[*]");
+	        JavaSparkContext sc = new JavaSparkContext(conf);
+	        
+	        List<String> inputStrings = Arrays.asList("Jack 12", "Foo 19", "Bar 36", "Captain 9");
+	        JavaRDD<String> regularRDD = sc.parallelize(inputStrings);
+	        
+	        // Method 1
+	        JavaPairRDD pairRDD1 = regularRDD.mapToPair(getNameAndAgePair());
+	        pairRDD1.coalesce(1).saveAsTextFile("output/pairRDD1.out");
+	       
+	        // Method 2
+	        JavaPairRDD<String,Integer> pairRDD2 = regularRDD.mapToPair(s->new Tuple2<>(s.split(" ")[0], Integer.valueOf(s.split(" ")[1])));
+	        pairRDD2.coalesce(1).saveAsTextFile("output/pairRDD2.out");
+	        
+	        // Method 3
+	        JavaPairRDD<String,Integer> pairRDD3 = regularRDD.mapToPair(s->new Tuple2<>(getKey(s), getValue(s)));
+	        pairRDD3.coalesce(1).saveAsTextFile("output/pairRDD3.out");
+	        
+	        
+	        logger.info("=== CONVERT TO PAIR RDD TEST END");
+			
+
+		} catch (Exception e) {
+			logger.error("Error", e);
+		}
+	}
+	
+	// PairFunction<T, K, V>
+	// T=type in input RDD
+	// K=Key type in Tuple
+	// V=Value Type in Tuple
+	private static PairFunction<String,String,Integer> getNameAndAgePair() {
+		return (PairFunction<String,String,Integer>) s -> new Tuple2<>(s.split(" ")[0], Integer.valueOf(s.split(" ")[1]));
+	}
+	
+	private static String getKey(String entry) {
+		String[] splits = entry.split(" ");
+		return splits[0];
+		
+	}
+	private static Integer getValue(String entry) {
+		String[] splits = entry.split(" ");
+		return Integer.valueOf(splits[1]);
+	}
+	
 	
 	
 }
