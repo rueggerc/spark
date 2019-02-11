89f84fdeac3fd11344f290e973988613400eeff4
Chris <chris.ruegger@gmail.com>
2019-02-11 03:36:52 -0500

    Stream

--- Spark3/scripts/stream/consumeKafkaWriteToPostgres.sh
+++ Spark3/scripts/stream/consumeKafkaWriteToPostgres.sh
@@ -2,6 +2,6 @@
 
 MASTER=spark://captain:7077
 JAR_FILE=/home/hadoop/code/spark/Spark3/target/Spark3-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-CLASS=com.rueggerllc.spark.stream.ConsumeKafkaWriteToPostgres
+CLASS=com.rueggerllc.spark.stream.structured.ConsumeKafkaWriteToPostgres
 
 spark-submit --class $CLASS --master $MASTER $JAR_FILE
